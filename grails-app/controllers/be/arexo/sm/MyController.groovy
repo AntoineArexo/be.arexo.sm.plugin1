67f6790a8cae3107b5fe7e3d10abe9030dea59de
Antoine Dubois <antoine.dubois@arexo.be>
2013-02-20 21:16:06 +0100

    global commit after dependencies set

--- grails-app/controllers/be/arexo/sm/MyController.groovy
+++ grails-app/controllers/be/arexo/sm/MyController.groovy
@@ -2,11 +2,13 @@ package be.arexo.sm
 
 import org.codehaus.groovy.grails.commons.DefaultGrailsApplication;
 import org.codehaus.groovy.grails.exceptions.GrailsConfigurationException;
+import org.codehaus.groovy.grails.plugins.DomainClassGrailsPlugin;
 
 class MyController {
 	def index(){
 
 		DefaultGrailsApplication
+		DomainClassGrailsPlugin
 		render "Top controller!jdsk"
 
 	}
